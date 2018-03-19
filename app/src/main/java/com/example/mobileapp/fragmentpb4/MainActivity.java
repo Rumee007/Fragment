bce55fc75fb177c3be2cbcb05efb072e9487de7a
rumee007 <37110449+Rumee007@users.noreply.github.com>
2018-03-19 23:37:17 +0600

    review code.

--- app/src/main/java/com/example/mobileapp/fragmentpb4/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/fragmentpb4/MainActivity.java
@@ -17,12 +17,17 @@ public class MainActivity extends AppCompatActivity implements FragmentOne.Messa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bundle = new Bundle();
+
 
         Log.e(TAG, "Activity: onCreate called");
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         FragmentOne fragmentOne = FragmentOne.getInstance("Hello Fragment One");
+        // Pass value with bundle
+        //bundle = new Bundle();
+        //bundle.putString("Yes", "Yes");
+        //fragmentOne.setArguments(bundle);
+
         ft.add(R.id.fragmentContainer,fragmentOne);
         ft.addToBackStack(null);
         ft.commit();
